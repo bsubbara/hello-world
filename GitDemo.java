8dd80ff944b82923f8f9e0ab057d6bdc19300389
bsubbara <79716444+bsubbara@users.noreply.github.com>
2021-03-02 22:39:44 -0700

    Add name and edit addNumber()
    Bhavani Subbaraman
    public GitDemo()
    private int addNumber(int num)
    private int reverseNumber(int num)

---
+++ GitDemo.java
@@ -0,0 +1,28 @@
+
+//Bhavani Subbaraman
+
+public class GitDemo {
+
+  private int previousNumber;
+  private int reverseNumber;
+  
+  public GitDemo() {
+    previousNumber = 1;
+  }
+  
+  private int addNumber(int num) {
+    return (previousNumber + num);
+  }
+  
+  private int reverseNumber(int num) {
+    int newNumber = 0;
+    while (num != 0) {
+      lastDigit = num % 10;
+      newNumber = (newNumber * 10) + lastDigit;
+      num = num/10;
+    }
+    reverseNum = newNumber;
+    return reverseNum;
+  }
+  
+}
